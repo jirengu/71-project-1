058848626a36390c7f9b4210e419176dd09689c4
jirengu <hunger@jirengu.com>
2022-11-30 16:16:22 +0800

    完整功能

--- js/main.js
+++ js/main.js
@@ -7,33 +7,38 @@ const searchMap = {
 }
 let searchType = 'baidu'
 let pageStatus = 'setting'   // preview
-const data = [{
-  title: '常用网站',
-  data: [
-    {
-      name: '饥人谷1',
-      url: 'https://jirengu.com'
-    },
-    {
-      name: '知乎',
-      url: 'https://zhihu.com'
-    }
-  ]
-},{
-  title: '精品博客',
-  data: [
-    {
-      name: '阮一峰',
-      url: 'https://javascript.ruanyifeng.com'
-    },
-    {
-      name: '若愚',
-      url: 'https://zhuanlan.zhihu.com/study-fe'
-    }
-  ]
-}] 
 
+let curModifyIdx = 0
+let $curModify = null
 
+let data = []
+// const data = [{
+//   title: '常用网站',
+//   data: [
+//     {
+//       name: '饥人谷1',
+//       url: 'https://jirengu.com'
+//     },
+//     {
+//       name: '知乎',
+//       url: 'https://zhihu.com'
+//     }
+//   ]
+// },{
+//   title: '精品博客',
+//   data: [
+//     {
+//       name: '阮一峰',
+//       url: 'https://javascript.ruanyifeng.com'
+//     },
+//     {
+//       name: '若愚',
+//       url: 'https://zhuanlan.zhihu.com/study-fe'
+//     }
+//   ]
+// }] 
+
+load()
 
 $$('.search-tab').forEach($tab => {
   console.log($tab)
@@ -72,6 +77,14 @@ $('.modal-1 .cancel').onclick = function() {
   $('.modal-1').classList.remove('show')
 }
 
+$('.modal-2 .cancel').onclick = function() {
+  $('.modal-2').classList.remove('show')
+}
+
+$('.modal-3 .cancel').onclick = function() {
+  $('.modal-3').classList.remove('show')
+}
+
 $('.modal-1 .confirm').onclick = function() {
   let value = $('.modal-1 input').value
   if(value === '') {
@@ -84,23 +97,95 @@ $('.modal-1 .confirm').onclick = function() {
   }
   data.push(obj)
   render(data)
+  save()
   $('.modal-1').classList.remove('show')
+}
+
+
+$('.modal-2 .confirm').onclick = function() {
+  let value = $('.modal-2 input').value
+  if(value === '') {
+    alert('输入不能为空')
+    return
+  }
+  data[curModifyIdx].title = value
+  render(data)
+  save()
+  $('.modal-2').classList.remove('show')
+}
+
+$('.modal-3 .confirm').onclick = function() {
+  let name = $('.modal-3 .site-name').value
+  let url = $('.modal-3 .site-url').value
+  if(name === '' || url === '') {
+    alert('输入不能为空')
+    return
+  }
+  data[curModifyIdx].data.push({name, url})
+  render(data)
+  save()
+  $('.modal-3').classList.remove('show')
 }
 
 
 $('.list').onclick = function(e) {
   console.log(e.path)
   let $delete = e.path.find($node => $node.classList&&$node.classList.contains('icon-delete'))
-  if(!$delete) return
-  let $result = e.path.filter($node => $node.classList&&$node.classList.contains('item'))
-  if($result.length > 0) {
-    let $item = $result[0]
-    let index = [...$$('.item')].indexOf($item)
-    console.log(index)
-    data.splice(index, 1)
-    render(data)
+  if($delete) {
+    let $result = e.path.filter($node => $node.classList&&$node.classList.contains('item'))
+    if($result.length > 0) {
+      let $item = $result[0]
+      let index = [...$$('.item')].indexOf($item)
+      console.log(index)
+      data.splice(index, 1)
+      render(data)
+      save()
+    }
+    console.log($result )
   }
-  console.log($result )
+  let $edit = e.path.find($node => $node.classList&&$node.classList.contains('icon-edit'))
+  if($edit) {
+    let $result = e.path.filter($node => $node.classList&&$node.classList.contains('item'))
+    if($result.length > 0) {
+      let $item = $result[0]
+      let index = [...$$('.item')].indexOf($item)
+      curModifyIdx = index
+      $curModify = $item
+      console.log(index)
+      $('.modal-2').classList.add('show')
+      let title = $item.querySelector('h2').innerText
+      $('.modal-2 input').value = title
+    }
+  }
+  let $add = e.path.find($node => $node.classList&&$node.classList.contains('icon-add'))
+  if($add) {
+    let $result = e.path.filter($node => $node.classList&&$node.classList.contains('item'))
+    if($result.length > 0) {
+      let $item = $result[0]
+      let index = [...$$('.item')].indexOf($item)
+      console.log(index)
+      $('.modal-3').classList.add('show')
+      curModifyIdx = index
+      $curModify = $item
+    }
+  }
+  let $remove = e.path.find($node => $node.classList&&$node.classList.contains('icon-remove')) 
+  if($remove) {
+    let $tag = $remove.parentElement
+    let tagArr = [...$tag.parentElement.children]
+    let tagIndex = tagArr.indexOf($tag)
+    console.log(tagIndex)
+    window.$remove = $remove
+    console.log($tag)
+    let $result = e.path.filter($node => $node.classList&&$node.classList.contains('item'))
+    if($result.length > 0) {
+      let $item = $result[0]
+      let index = [...$$('.item')].indexOf($item)
+      data[index].data.splice(tagIndex, 1)
+      save()
+      $tag.remove()
+    }
+  } 
 }
 
 function search() {
@@ -164,8 +249,9 @@ render(data)
         <li class="item">
           <h2>常用网站<span></span></h2>
           <ul class="panel">
-            <li class="tag"><a href="" target="_blank">百度</a> </li>
-            <li class="tag"><a href="" target="_blank">百度</a></li>
+            <li class="tag"><svg class="icon icon-remove" aria-hidden="true"><use xlink:href="#icon-delete"></use></svg><a href="" target="_blank">百度</a> </li>
+            <li class="tag"><svg class="icon icon-remove" aria-hidden="true"><use xlink:href="#icon-delete"></use></svg><a href="" target="_blank">百度</a></li>
+            <li class="tag icon-add "><svg class="icon" aria-hidden="true"><use xlink:href="#icon-plus"></use></svg></li>
           </ul>
         </li>
 */
@@ -177,13 +263,14 @@ function render(data) {
     const $h2 = document.createElement('h2')
     $h2.append(obj.title)
     const $iconSpan = document.createElement('span')
-    $iconSpan.innerHTML = '<svg class="icon icon-delete" aria-hidden="true"><use xlink:href="#icon-delete"></use>'
+    $iconSpan.innerHTML = '<svg class="icon icon-delete" aria-hidden="true"><use xlink:href="#icon-delete"></use></svg><svg class="icon icon-edit" aria-hidden="true"><use xlink:href="#icon-edit"></use></svg>'
     $h2.append($iconSpan)
     const $ul = document.createElement('ul')
     $ul.classList.add('panel')
     let $liArray = obj.data.map(item => {
       const $tag = document.createElement('li')
       $tag.classList.add('tag')
+      $tag.innerHTML = '<svg class="icon icon-remove" aria-hidden="true"><use xlink:href="#icon-delete"></use></svg>'
       const $a = document.createElement('a')
       $a.setAttribute('href', item.url)
       $a.setAttribute('target', '_blank')
@@ -192,6 +279,11 @@ function render(data) {
       return $tag
     })
     $ul.append(...$liArray)
+    let $icon = document.createElement('li')
+    $icon.classList.add('tag')
+    $icon.classList.add('icon-add')
+    $icon.innerHTML = '<svg class="icon icon-add" aria-hidden="true"><use xlink:href="#icon-plus"></use></svg>'
+    $ul.append($icon)
     $item.append($h2, $ul)
     return $item
   })
@@ -202,3 +294,16 @@ function render(data) {
   $('#websites .list').append(...$itemArr)
 }
 
+
+function save() {
+  localStorage.setItem('website', JSON.stringify(data))
+}
+
+function load() {
+  let storagedData = localStorage.getItem('website')
+  if(storagedData) {
+    data = JSON.parse(storagedData)
+  } else {
+    data = []
+  }
+}
